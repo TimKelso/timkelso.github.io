a86030a4d5cd7cf954c54584ebf27b8fb595a78e
Tim <136618985+TimKelso@users.noreply.github.com>
2024-11-19 21:43:03 +0100

    Wipe out the whole project for a fresh start

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { reels } from './data/reels';
-import { createReel } from './components/reel';
-
-const reelsContainer = document.getElementById('reels-container');
-
-reelsContainer.innerHTML = reels.map(createReel).join('');
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/portfolio/',
-  css: {
-    postcss: './postcss.config.js',
-  },
-});
