e46d526fcb98f435cfa38f4bcb4511fd8ad79a87
Tim <136618985+TimKelso@users.noreply.github.com>
2024-11-24 18:11:21 +0100

    Add toggle functionality to display more info about each project

--- src/components/feature/portfolio/Project.jsx
+++ src/components/feature/portfolio/Project.jsx
@@ -1,5 +1,6 @@
 // ==================== IMPORTS ===============================================
 
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Image from './Image';
 import Button from './Button';
@@ -13,42 +14,54 @@ const toggleSaveProject = (title) => {
   console.log(`Saved project: ${title}`);
 };
 
-const showMoreInfo = (descriptionLong) => {
-  console.log(descriptionLong);
-};
+const Project = ({ date, title, descriptionHook, descriptionLong, tags, imagePath, projectURL, githubURL }) => {
+  const [showMoreInfo, setShowMoreInfo] = useState(false);
 
-const Project = ({ date, title, descriptionHook, descriptionLong, tags, imagePath, projectURL, githubURL }) => (
+  const toggleMoreInfo = () => setShowMoreInfo(!showMoreInfo);
+
   // --------------------- JSX ------------------------------------------------
-  <>
-    <HorizontalLine />
-    <article className="flex h-dvh flex-row justify-between">
-      <div id="timeline">vertical line</div>
+  return (
+    <>
+      <HorizontalLine />
+      <article className="flex h-dvh flex-row justify-between">
+        <div id="timeline">vertical line</div>
 
-      <div id="content" className="my-7 flex max-w-[31.25rem] flex-col gap-5">
-        <Image imagePath={imagePath} alt={`Screenshot of ${title}`} />
-        <div className="flex flex-col gap-3">
-          <h5 className="font-mono text-base tracking-widest text-gray-500">{date}</h5>
-          <h4 className="text-3xl">{title}</h4>
-          <p className="text-pretty text-base">{descriptionHook}</p>
-          <Tags tags={tags} />
+        <div id="content" className="my-7 flex max-w-[31.25rem] flex-col gap-5">
+          {!showMoreInfo ? <Image imagePath={imagePath} alt={`Screenshot of ${title}`} /> : null}
+          <div className="flex h-full flex-col gap-3">
+            <h5 className="font-mono text-base tracking-widest text-gray-500">{date}</h5>
+            <h4 className="text-3xl">{title}</h4>
+            {!showMoreInfo ? (
+              <p className="truncate text-pretty text-base">{descriptionHook}</p>
+            ) : (
+              <p className="truncate text-pretty text-base">
+                {descriptionHook} {descriptionLong}
+              </p>
+            )}
+            <Tags tags={tags} />
+          </div>
         </div>
-      </div>
 
-      <div id="action" className="flex flex-col justify-between">
-        <div className="flex flex-col gap-1">
-          <Button icon="more_vert" onClick={() => console.log('Opened more buttons')} />
-          <Button icon="bookmarks" onClick={() => window.open(projectURL)} />
+        <div id="action" className="flex flex-col justify-between">
+          <div className="flex flex-col gap-1">
+            <Button icon="more_vert" onClick={() => console.log('Opened more buttons')} />
+            <Button icon="bookmarks" onClick={() => window.open(projectURL)} />
+          </div>
+          <div className="flex flex-col gap-1">
+            <Button icon="bookmark" label="Save" onClick={() => toggleSaveProject(title)} />
+            {!showMoreInfo ? (
+              <Button icon="info" label="Info" onClick={() => toggleMoreInfo()} />
+            ) : (
+              <Button icon="cancel" label="Info" onClick={() => toggleMoreInfo()} />
+            )}
+            {githubURL && <Button icon="code" label="Code" onClick={() => window.open(githubURL, '_blank')} />}
+            {projectURL && <Button icon="open_in_new" label="Open" onClick={() => window.open(projectURL, '_blank')} />}
+          </div>
         </div>
-        <div className="flex flex-col gap-1">
-          <Button icon="bookmark" label="Save" onClick={() => toggleSaveProject(title)} />
-          <Button icon="info" label="Info" onClick={() => showMoreInfo(descriptionLong)} />
-          {githubURL && <Button icon="code" label="Code" onClick={() => window.open(githubURL, '_blank')} />}
-          {projectURL && <Button icon="open_in_new" label="Open" onClick={() => window.open(projectURL, '_blank')} />}
-        </div>
-      </div>
-    </article>
-  </>
-);
+      </article>
+    </>
+  );
+};
 
 // ==================== PROP TYPES ============================================
 
--- src/components/feature/portfolio/Section.jsx
+++ src/components/feature/portfolio/Section.jsx
@@ -14,7 +14,7 @@ const Section = () => {
       title: 'Nebuline',
       descriptionHook: 'A sleek and interactive website for a fictional space tourism company.',
       descriptionLong:
-        'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum.',
+        'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum. lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum.lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum.lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum.lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum.lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum.lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum.',
       tags: ['Website', 'Design', 'Corporate', 'UI'],
       imagePath: 'assets/images/portfolio/solo_projects/nebuline/section_2',
       projectURL: 'https://itgwebb.se/klass/te21h/tim/slutprojekt/index.html',
@@ -24,7 +24,7 @@ const Section = () => {
       title: "Tim's 2048",
       descriptionHook: 'A clone of the popular 2048 puzzle game with a personal touch.',
       descriptionLong:
-        'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum.',
+        'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum.lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum.',
       tags: ['Game', 'Clone', 'JavaScript', 'UX'],
       imagePath: 'assets/images/portfolio/solo_projects/tims_2048',
       projectURL: 'https://nti-tim.github.io/Tims-2048',
@@ -43,15 +43,15 @@ const Section = () => {
   ];
 
   const groupProjects = [
-    {
-      date: '18 May 2023',
-      title: 'TESTING LONG TITLE FOR GROUP PROJECT',
-      descriptionHook: 'A sleek and interactive website for a fictional space tourism company.',
-      descriptionLong:
-        'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum.',
-      tags: ['Website', 'Design', 'Corporate', 'UI', 'Group', 'Team', 'Collaboration'],
-      imagePath: 'assets/images/portfolio/solo_projects/nebuline/section_1',
-    },
+    // {
+    //   date: '18 May 2023',
+    //   title: 'TESTING LONG TITLEFORGROUPPROJECT',
+    //   descriptionHook: 'A sleek and interactive website for a fictional space tourism companycompanycompanycompanycompanycompany.',
+    //   descriptionLong:
+    //     'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisinisinisinisinisinisinisinisinisinisinisinisinisinisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt culpa qui officia deserunt mollit anim id est laborum.',
+    //   tags: ['Website', 'Design', 'Corporate', 'UI', 'Group', 'Team', 'Collaboration', 'ProjectVeryLongTagNameExample', 'HTML', 'CSS', 'JavaScript'],
+    //   imagePath: 'assets/images/portfolio/solo_projects/nebuline/section_1',
+    // },
   ];
 
   return (
