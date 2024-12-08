ad85946128264e0f15c2e496c448ffdef4cda8d1
Tim <136618985+TimKelso@users.noreply.github.com>
2024-12-08 20:16:15 +0100

    New style for the bookmarks dropdown

--- src/components/feature/portfolio/Bookmarks.jsx
+++ src/components/feature/portfolio/Bookmarks.jsx
@@ -8,27 +8,30 @@ const Bookmarks = ({ onScrollToProject }) => {
   const [menuVisible, setMenuVisible] = useState(false);
 
   return (
-    <div className="relative">
+    <div>
       <Button icon="bookmarks" label={bookmarks.length > 0 ? String(bookmarks.length) : ''} onClick={() => setMenuVisible((prev) => !prev)} />
 
       {menuVisible && (
-        <ul className="absolute right-0 border-gray-700 bg-gray-900">
+        <ul className="absolute right-0 select-none">
           {bookmarks.length > 0 ? (
             bookmarks.map((bookmark, index) => (
-              <li
-                key={index}
-                className="flex cursor-pointer items-center justify-between p-2 hover:bg-gray-800"
-                onClick={() => {
-                  setMenuVisible(false);
-                  onScrollToProject(bookmark.id);
-                }}
-              >
-                <span className="truncate text-white">{bookmark.title}</span>
-                <i className="material-symbols-outlined text-gray-400">bookmark</i>
+              <li key={index} className="mb-3">
+                <button
+                  className="flex items-center justify-self-end"
+                  onClick={() => {
+                    setMenuVisible(false);
+                    onScrollToProject(bookmark.id);
+                  }}
+                >
+                  <span className="w-full truncate rounded-l-full bg-gray-900 py-3 pl-3 text-sm">{bookmark.title}</span>
+                  <i className="material-symbols-outlined rounded-r-full bg-gray-900 px-2 py-1 text-3xl">bookmark</i>
+                </button>
               </li>
             ))
           ) : (
-            <li className="p-2 text-center text-gray-400">No bookmarks (this message is just temporary)</li>
+            <li className="mt-6">
+              <span className="w-full truncate rounded-full bg-gray-900 px-3 py-3 text-sm">No bookmarks</span>
+            </li>
             // If there are no bookmarks, highlight the save bookmark button
           )}
         </ul>
