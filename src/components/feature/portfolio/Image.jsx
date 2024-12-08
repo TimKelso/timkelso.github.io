bd0f0bad72096d2018034600bf3ea79fd2f1c198
Tim <136618985+TimKelso@users.noreply.github.com>
2024-12-08 21:29:12 +0100

    Add support for mono-themed images

--- src/components/feature/portfolio/Image.jsx
+++ src/components/feature/portfolio/Image.jsx
@@ -4,25 +4,36 @@ import PropTypes from 'prop-types';
 
 // ==================== COMPONENT =============================================
 
-const Image = ({ imagePath, alt }) => {
+const Image = ({ imagePath, alt, supportsVariants = false }) => {
   // --------------------- FUNCTIONS ------------------------------------------
 
-  const getImageSrc = (mode, ext) => `${imagePath}/${mode}/img.${ext}`;
+  // Generates image source paths for light/dark mode or fallback.
+  const getImageSrc = (mode, ext) => (supportsVariants ? `${imagePath}/${mode}/img.${ext}` : `${imagePath}/img.${ext}`);
 
   return (
-    // --------------------- JSX ------------------------------------------------
-
+    // --------------------- JSX ----------------------------------------------
     <>
-      <picture className="dark:hidden">
-        <source srcSet={getImageSrc('light_mode', 'avif')} type="image/avif" />
-        <source srcSet={getImageSrc('light_mode', 'webp')} type="image/webp" />
-        <img className="rounded-xl" src={getImageSrc('light_mode', 'jpg')} alt={alt} loading="lazy" />
-      </picture>
-      <picture className="hidden dark:block">
-        <source srcSet={getImageSrc('dark_mode', 'avif')} type="image/avif" />
-        <source srcSet={getImageSrc('dark_mode', 'webp')} type="image/webp" />
-        <img className="rounded-xl" src={getImageSrc('dark_mode', 'jpg')} alt={alt} loading="lazy" />
-      </picture>
+      {supportsVariants ? (
+        <>
+          <picture className="dark:hidden">
+            <source srcSet={getImageSrc('light_mode', 'avif')} type="image/avif" />
+            <source srcSet={getImageSrc('light_mode', 'webp')} type="image/webp" />
+            <img className="rounded-xl" src={getImageSrc('light_mode', 'jpg')} alt={alt} loading="lazy" />
+          </picture>
+
+          <picture className="hidden dark:block">
+            <source srcSet={getImageSrc('dark_mode', 'avif')} type="image/avif" />
+            <source srcSet={getImageSrc('dark_mode', 'webp')} type="image/webp" />
+            <img className="rounded-xl" src={getImageSrc('dark_mode', 'jpg')} alt={alt} loading="lazy" />
+          </picture>
+        </>
+      ) : (
+        <picture>
+          <source srcSet={getImageSrc('', 'avif')} type="image/avif" />
+          <source srcSet={getImageSrc('', 'webp')} type="image/webp" />
+          <img className="rounded-xl" src={getImageSrc('', 'jpg')} alt={alt} loading="lazy" />
+        </picture>
+      )}
     </>
   );
 };
@@ -32,6 +43,7 @@ const Image = ({ imagePath, alt }) => {
 Image.propTypes = {
   imagePath: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
+  supportsVariants: PropTypes.bool, // Indicates whether dark/light mode variants are supported.
 };
 
 // ==================== EXPORTS ===============================================
--- src/components/feature/portfolio/Project.jsx
+++ src/components/feature/portfolio/Project.jsx
@@ -7,7 +7,7 @@ import Button from './Button';
 import Tags from './Tags';
 import HorizontalLine from '../../global/common/HorizontalLine';
 
-const Project = ({ date, title, descriptionHook, descriptionLong, tags, imagePath, projectURL, githubURL }) => {
+const Project = ({ date, title, descriptionHook, descriptionLong, tags, imagePath, supportsVariants, projectURL, githubURL }) => {
   const projectId = `portfolio-${title.toLowerCase().replace(/\s+/g, '-')}`;
   const { bookmarks, toggleBookmark } = BookmarkUsage();
   const isBookmarked = bookmarks.some((bookmark) => bookmark.id === projectId);
@@ -27,7 +27,7 @@ const Project = ({ date, title, descriptionHook, descriptionLong, tags, imagePat
         <div id="timeline">vertical line</div>
 
         <div id="content" className="my-7 flex max-w-[31.25rem] flex-col gap-5">
-          {!showMoreInfo ? <Image imagePath={imagePath} alt={`Screenshot of ${title}`} /> : null}
+          {!showMoreInfo ? <Image imagePath={imagePath} alt={`Screenshot of ${title}`} supportsVariants={supportsVariants} /> : null}
           <div className="flex h-full flex-col gap-3">
             <h5 className="font-mono text-base tracking-widest text-gray-500">{date}</h5>
             <h4 className="text-3xl">{title}</h4>
@@ -67,6 +67,7 @@ Project.propTypes = {
   descriptionLong: PropTypes.string.isRequired,
   tags: PropTypes.arrayOf(PropTypes.string).isRequired,
   imagePath: PropTypes.string.isRequired,
+  supportsVariants: PropTypes.bool,
   projectURL: PropTypes.string || null,
   githubURL: PropTypes.string || null,
 };
