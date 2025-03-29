d5e7090ece39a41018e66f22c32b9b07912c8f38
Tim <136618985+TimKelso@users.noreply.github.com>
2025-03-29 17:33:06 +0100

    Create an aesthetic glow effect for the project images

--- src/components/feature/portfolio/Image.jsx
+++ src/components/feature/portfolio/Image.jsx
@@ -3,22 +3,25 @@ import PropTypes from 'prop-types';
 const Image = ({ imagePath, alt, supportsVariants = false }) => {
   const getImageSrc = (mode, ext) => (supportsVariants ? `${imagePath}/${mode}/img.${ext}` : `${imagePath}/img.${ext}`);
 
-  const imgClasses = 'rounded-xl max-h-[40svh] mx-auto';
+  const imgClasses = 'rounded-xl max-h-[40svh]';
+  const blurImgClasses = 'rounded-xl max-h-[40svh] inset-0 absolute -z-1 scale-90 blur-3xl saturate-200';
 
   return (
-    <>
+    <div className="relative mx-auto">
       {supportsVariants ? (
         <>
           <picture className="dark:hidden">
             <source srcSet={getImageSrc('light_mode', 'avif')} type="image/avif" />
             <source srcSet={getImageSrc('light_mode', 'webp')} type="image/webp" />
             <img className={imgClasses} src={getImageSrc('light_mode', 'jpg')} alt={alt} loading="lazy" />
+            <img className={blurImgClasses} src={getImageSrc('light_mode', 'jpg')} aria-hidden="true" alt="" loading="lazy" />
           </picture>
 
           <picture className="hidden dark:block">
             <source srcSet={getImageSrc('dark_mode', 'avif')} type="image/avif" />
             <source srcSet={getImageSrc('dark_mode', 'webp')} type="image/webp" />
             <img className={imgClasses} src={getImageSrc('dark_mode', 'jpg')} alt={alt} loading="lazy" />
+            <img className={blurImgClasses} src={getImageSrc('dark_mode', 'jpg')} aria-hidden="true" alt="" loading="lazy" />
           </picture>
         </>
       ) : (
@@ -26,9 +29,10 @@ const Image = ({ imagePath, alt, supportsVariants = false }) => {
           <source srcSet={getImageSrc('', 'avif')} type="image/avif" />
           <source srcSet={getImageSrc('', 'webp')} type="image/webp" />
           <img className={imgClasses} src={getImageSrc('', 'jpg')} alt={alt} loading="lazy" />
+          <img className={blurImgClasses} src={getImageSrc('', 'jpg')} aria-hidden="true" alt="" loading="lazy" />
         </picture>
       )}
-    </>
+    </div>
   );
 };
 
