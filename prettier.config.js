facac135119a3858567c0827103e5320abf22062
Tim <136618985+TimKelso@users.noreply.github.com>
2025-05-11 21:35:59 +0200

    Refactor: Migrate Prettier configuration from .prettierrc.json to prettier.config.js for consistency

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 150,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+};
